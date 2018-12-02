af065f66139c387af57937a6223f5338590095f9
Darryl Abbate <rootbeersoup@gmail.com>
2018-12-01 21:46:03 -0600

    Add part 2

--- day01/main.c
+++ day01/main.c
@@ -3,19 +3,40 @@
 
 #define SIZE 10
 
-int find_freq(int starting_freq, FILE *input)
+int find_freq(FILE *input)
 {
-  int freq = starting_freq;
+  int freq = 0;
   char change[SIZE];
   while(fgets(change, SIZE, input) != NULL)
     freq += atoi(change);
   return freq;
 }
 
+int find_repeat_freq(FILE *input)
+{
+  int freq = 0;
+  int hist[200000];
+  char change[SIZE];
+  int i = 0;
+  while (i < 200000) {
+    rewind(input);
+    while (fgets(change, SIZE, input)) {
+      hist[i] = freq;
+      freq += atoi(change);
+      int j;
+      for(j = i+1; j >= 0; j--)
+        if (freq == hist[j])
+          return freq;
+    i++;
+    }
+  }
+  return freq;
+}
+
 int main(void)
 {
-  FILE *input;
-  input = fopen("input", "r");
-  printf("Part 1: %d\n", find_freq(0, input));
+  FILE *input = fopen("input", "r");
+  printf("Part 1: %d\n", find_freq(input));
+  printf("Part 2: %d\n", find_repeat_freq(input));
   return 0;
 }
