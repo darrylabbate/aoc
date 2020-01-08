1a5a1e627ceb53346a1f9de8549b41e0847af03f
Darryl Abbate <rootbeersoup@gmail.com>
2020-01-08 15:50:38 -0600

    [15/07] Add solutions

---
+++ 15/07/2.pl
@@ -0,0 +1,33 @@
+while (<>) {
+    /(.*) -> (.*)/;
+    $w{$2} = $1;
+}
+
+_eval('a');
+$w{'b'} = $s{'a'};
+%s = {};
+print _eval('a'), "\n";
+
+sub _eval {
+    my $idx = shift;
+    return $s{$idx} if ($s{$idx});
+    return $idx     if ($idx =~ /^\d+$/);
+    my $exp = $w{$idx};
+    if ($exp =~ /^(\w+)$/) {
+        $s{$idx} = _eval($1);
+    } elsif ($exp =~ /NOT (\w+)/) {
+        $s{$idx} = ~_eval($1);
+    } else {
+        $exp =~ /(.*) (.*) (.*)/;
+        my $x  = $1;
+        my $op = $2;
+        my $y  = $3;
+        $x = _eval($x);
+        $y = _eval($y);
+        if    ($op eq 'AND'   ) { $s{$idx} = $x &  $y; }
+        elsif ($op eq 'OR'    ) { $s{$idx} = $x |  $y; }
+        elsif ($op eq 'RSHIFT') { $s{$idx} = $x >> $y; }
+        elsif ($op eq 'LSHIFT') { $s{$idx} = $x << $y; }
+    }
+    return $s{$idx};
+}
